41c8056142b676a4da62cbce81ec429d7a98cad3
Moritz Sauter <SauterMoritz@gmx.de>
2017-10-26 09:10:59 +0200

    implemented fibonacci function

--- lab1/lab1-example/src/main/java/de/hska/iwi/vislab/lab1/example/ws/FibonacciService.java
+++ lab1/lab1-example/src/main/java/de/hska/iwi/vislab/lab1/example/ws/FibonacciService.java
@@ -1,7 +1,13 @@
 package de.hska.iwi.vislab.lab1.example.ws;
 
-public class FibonacciService{
-    public int getFibonacci(int i) {
-        return 0;
+public class FibonacciService {
+    public int getFibonacci(int n) {
+        int i = 0, j = 1, k, t;
+        for (k = 1; k <= n; ++k) {
+            t = i + j;
+            i = j;
+            j = t;
+        }
+        return j;
     }
 }
