61ffa50f575a6d776b5e086a0cda118cc3495cc5
Marvin Rühe <marvin@server-fish.de>
2017-10-26 09:32:22 +0200

    finish

--- lab1/lab1-example/src/main/java/de/hska/iwi/vislab/lab1/example/ws/FibonacciService.java
+++ lab1/lab1-example/src/main/java/de/hska/iwi/vislab/lab1/example/ws/FibonacciService.java
@@ -1,13 +1,9 @@
 package de.hska.iwi.vislab.lab1.example.ws;
 
-public class FibonacciService {
-    public int getFibonacci(int n) {
-        int i = 0, j = 1, k, t;
-        for (k = 1; k <= n; ++k) {
-            t = i + j;
-            i = j;
-            j = t;
-        }
-        return j;
-    }
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "de.hska.iwi.vislab.lab1.example.ws.FibonacciService")
+public interface FibonacciService {
+    public int getFibonacci(@WebParam(name = "index")int n);
 }
--- lab1/lab1-example/src/main/java/de/hska/iwi/vislab/lab1/example/ws/FibonacciServiceImpl.java
+++ lab1/lab1-example/src/main/java/de/hska/iwi/vislab/lab1/example/ws/FibonacciServiceImpl.java
@@ -2,11 +2,17 @@ package de.hska.iwi.vislab.lab1.example.ws;
 
 import javax.jws.WebService;
 
-@WebService(endpointInterface = "de.hska.iwi.vislab.lab1.example.ws.FibonacciServiceImpl")
-public class FibonacciServiceImpl implements FibonacciServiceIntf {
+@WebService(endpointInterface = "de.hska.iwi.vislab.lab1.example.ws.FibonacciService")
+public class FibonacciServiceImpl implements FibonacciService {
 
     @Override
-    public int getFibonacci(int index) {
-        return 0;
+    public int getFibonacci(int n) {
+        int i = 0, j = 1, k, t;
+        for (k = 1; k < n; ++k) {
+            t = i + j;
+            i = j;
+            j = t;
+        }
+        return j;
     }
 }
