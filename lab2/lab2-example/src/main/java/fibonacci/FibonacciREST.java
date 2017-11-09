adc04dfe49e7a524c81ba6b68017f1a46d6af7b9
Kevin <kevthevirus@yahoo.de>
2017-11-09 09:35:36 +0100

    Done with exercise 2

--- lab2/lab2-example/src/main/java/fibonacci/FibonacciREST.java
+++ lab2/lab2-example/src/main/java/fibonacci/FibonacciREST.java
@@ -1,49 +1,113 @@
 package fibonacci;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Root resource (exposed at "fibonacci" path)
  */
-@Path("/fibonacci/{position}")
+@Path("fibonacci")
 public class FibonacciREST {
+	
+	private static final int DEFAULT_STATE = 0;
+	private static int state = DEFAULT_STATE;
 
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return The fibonacci number at the given position.
-     */
-    
+	/**
+	 * Method handling HTTP GET requests. The returned object will be sent to the
+	 * client as "text/plain" media type.
+	 *
+	 * @return The fibonacci number at the given position.
+	 */
+
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
+	@Path("{position}")
 	public String getFibonacciAtPosition(@PathParam("position") String position) {
-		
-		if(position.matches("\\d")) {
-			int number = getFibonacci(Integer.parseInt(position));
-			return "" + number;			
+
+		if (position.matches("\\d+")) {
+			state = Integer.parseInt(position);
+			return "" + getFibonacci(state);
 		} else {
-			return "Invalid format!";
+			return "Invalid format! Expected /position";
 		}
-		
+	}
+
+	/**
+	 * Handling REST request for a range of fibonacci numbers.
+	 * 
+	 * @param from
+	 *            First desired fibonnaci number
+	 * @param to
+	 *            Last desired fibonnaci number
+	 * @return A list of the desired fibonnaci numbers.
+	 */
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("{from}/{to}")
+	public String getFibonacciRange(@PathParam("from") String from, @PathParam("to") String to) {
+
+		if (from.matches("\\d+") && to.matches("\\d+")) {
+			String returnValue = "";
+
+			for (int i = Integer.parseInt(from); i <= Integer.parseInt(to); i++) {
+				returnValue += getFibonacci(i) + "\n";
+			}
+			return returnValue;
+		}
+		return "Invalid format! Expected /from/to";
 	}
 	
+	@POST
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("next")
+	public Response getNextFibonacci() {
+		state++;
+		return Response.ok().build();
+	}
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("")
+	public String getCurrentFibonacci() {
+		return "" + getFibonacci(state);
+	}
+	
+	@DELETE
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("")
+	public String resetFibonacci() {
+		state = DEFAULT_STATE;
+		return "" + getFibonacci(state);
+	}
+	
+	
+	
+
 	/**
 	 * Calculates the fibonacci number at given position.
-	 * @param position The desired position
+	 * 
+	 * @param position
+	 *            The desired position
 	 * @return The fibonacci number at the specified position.
 	 */
-    private int getFibonacci(int position) {
-        int i = 0, j = 1, k, t;
-        for (k = 1; k < position; ++k) {
-            t = i + j;
-            i = j;
-            j = t;
-        }
-        return j;
-    }
+	private int getFibonacci(int position) {
+		if (position == 0) {
+			return 0;
+		}
+
+		int i = 0, j = 1, k, t;
+		for (k = 1; k < position; ++k) {
+			t = i + j;
+			i = j;
+			j = t;
+		}
+		return j;
+	}
 }
--- lab2/lab2-example/src/test/java/de/hska/iwi/vislab/lab2/example/FibonacciTest.java
+++ lab2/lab2-example/src/test/java/de/hska/iwi/vislab/lab2/example/FibonacciTest.java
@@ -4,8 +4,10 @@ import static org.junit.Assert.assertEquals;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
@@ -43,19 +45,59 @@ public class FibonacciTest {
 	@Test
 	public void testFirst() {
 		String responseMsg = target.path("/fibonacci/0").request().accept(MediaType.TEXT_PLAIN).get(String.class);
-		assertEquals("1", responseMsg);
+		assertEquals("0", responseMsg);
 	}
 	
 	@Test
 	public void testNegative() {
 		String responseMsg = target.path("/fibonacci/-1").request().accept(MediaType.TEXT_PLAIN).get(String.class);
-		assertEquals("Invalid format!", responseMsg);
+		assertEquals("Invalid format! Expected /position", responseMsg);
 	}
 	
 	@Test
 	public void testNonInteger() {
 		String responseMsg = target.path("/fibonacci/abc").request().accept(MediaType.TEXT_PLAIN).get(String.class);
-		assertEquals("Invalid format!", responseMsg);
+		assertEquals("Invalid format! Expected /position", responseMsg);
 	}
+	
+	@Test
+	public void testList() {
+		String responseMsg = target.path("/fibonacci/0/5").request().accept(MediaType.TEXT_PLAIN).get(String.class);
+		assertEquals("0\n1\n1\n2\n3\n5\n", responseMsg);
+	}
+	
+	@Test
+	public void testListNonInteger() {
+		String responseMsg = target.path("/fibonacci/0/abc").request().accept(MediaType.TEXT_PLAIN).get(String.class);
+		assertEquals("Invalid format! Expected /from/to", responseMsg);
+	}
+	
+	@Test
+	public void testGET() {
+		String responseMsg = target.path("/fibonacci/").request().accept(MediaType.TEXT_PLAIN).get(String.class);
+		assertEquals("0", responseMsg);
+	}
+	
+	@Test
+	public void testNEXT() {
+		Response responseMsg = target.path("/fibonacci/next").request().accept(MediaType.TEXT_PLAIN).post(Entity.entity("", MediaType.TEXT_PLAIN));
+		assertEquals(Response.Status.OK.getStatusCode(), responseMsg.getStatus());
+	}
+	
+	@Test
+	public void testNEXTresult() {
+		Response responseMsg = target.path("/fibonacci/next").request().accept(MediaType.TEXT_PLAIN).post(Entity.entity("", MediaType.TEXT_PLAIN));
+		assertEquals(Response.Status.OK.getStatusCode(), responseMsg.getStatus());
+		String currentValue = target.path("/fibonacci/").request().accept(MediaType.TEXT_PLAIN).get(String.class);
+		assertEquals("1", currentValue);
+	}
+	
+	@Test
+	public void testDELETE() {
+		Response responseMsg = target.path("/fibonacci/").request().accept(MediaType.TEXT_PLAIN).delete();
+		assertEquals(Response.Status.OK.getStatusCode(), responseMsg.getStatus());
+	}
+	
+	
 	
 }
